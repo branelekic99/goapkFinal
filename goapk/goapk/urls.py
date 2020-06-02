1f27ce447a2016194faef848b4afa0832c434ef6
Branislav Lekic <blekic@planetsoft.ba>
2020-06-02 08:42:43 +0200

    url fix

--- goapk/goapk/urls.py
+++ goapk/goapk/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^odmor/',include('odmori.urls')),
     re_path(r'^api/',include('users.urls')),
-    re_path(r'^',TemplateView.as_view(template_name='index.html'))
+    re_path(r'',TemplateView.as_view(template_name='index.html'))
     
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
